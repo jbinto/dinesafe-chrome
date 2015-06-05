7b5cfd823000731669f52d6acc32897e49e80770
Jesse Buchanan <hello@jessebuchanan.ca>
2015-06-05 15:52:54 -0400

    Also select address

--- contentScript.js
+++ contentScript.js
@@ -5,23 +5,29 @@
   // TODO: detect site name from URL
   var siteName = "yelp";
 
-  var cleanName = function(dom) {
-    return dom.innerText.trim().toLowerCase();
+  var clean = function(selector) {
+    return selector.innerText.trim().toLowerCase();
     // TODO: remove apostrophes, parens, ampersands, hashes...
   };
 
   var selectors = {};
   switch (siteName) {
     case "yelp":
-      selectors.nameSelector = document.querySelector(".biz-page-title");
+      selectors.name = document.querySelector(".biz-page-title");
+
+      // TODO: this selector is lifted from Chrome dev tools "Copy CSS path".
+      // works for now, but might be worth making less specific for future
+      selectors.address = document.querySelector('#wrap > div.biz-country-ca > div > div.top-shelf > div > div.biz-page-subheader > div.mapbox-container > div > div.mapbox-text > ul > li.address > strong > address > span:nth-child(1)');
       break;
 
     default:
       throw "No selectors for " + siteName;
   }
 
-  var restaurantName = cleanName(selectors.nameSelector);
+  var name = clean(selectors.name);
+  var address = clean(selectors.address);
 
-  console.log("Restaurant name is: " + restaurantName);
+  // hooray! ES6 template literals!
+  console.log(`name: ${name}; address: ${address}`);
 
 })();
