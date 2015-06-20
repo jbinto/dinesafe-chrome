78d739956c31b86eec78b8a7e38db126909d04d1
Jesse Buchanan <hello@jessebuchanan.ca>
2015-06-19 22:22:40 -0400

    Refactor initial Yelp selector logic. Basic fetch, but blocked by CORS. [..]
    > Fetch API cannot load http://dinesafe.dev/api/1.0/establishments.json?search=black%20hoof. No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://www.yelp.ca' is therefore not allowed access.

--- contentScript.js
+++ contentScript.js
@@ -1,33 +1,42 @@
 (function() {
+  "use strict";
+
+  const API_URL = "http://dinesafe.dev/api/1.0/establishments.json"
   
-  console.log("extension running");
+  console.log("dinesafe-chrome extension running");
 
-  // TODO: detect site name from URL
-  var siteName = "yelp";
-
-  var clean = function(selector) {
-    return selector.innerText.trim().toLowerCase();
+  let clean = function(selector) {
+    return selector.innerText
+      .trim()
+      .toLowerCase();
     // TODO: remove apostrophes, parens, ampersands, hashes...
   };
 
-  var selectors = {};
-  switch (siteName) {
-    case "yelp":
-      selectors.name = document.querySelector(".biz-page-title");
+  let yelp = function() {
+    let name = document.querySelector(".biz-page-title");
 
-      // TODO: this selector is lifted from Chrome dev tools "Copy CSS path".
-      // works for now, but might be worth making less specific for future
-      selectors.address = document.querySelector('#wrap > div.biz-country-ca > div > div.top-shelf > div > div.biz-page-subheader > div.mapbox-container > div > div.mapbox-text > ul > li.address > strong > address > span:nth-child(1)');
-      break;
+    // TODO: this selector is lifted from Chrome dev tools "Copy CSS path".
+    // works for now, but might be worth making less specific for future
+    let address = document.querySelector('#wrap > div.biz-country-ca > div > div.top-shelf > div > div.biz-page-subheader > div.mapbox-container > div > div.mapbox-text > ul > li.address > strong > address > span:nth-child(1)');
 
-    default:
-      throw "No selectors for " + siteName;
+    return {
+      name: clean(name),
+      address: clean(address)
+    }
   }
 
-  var name = clean(selectors.name);
-  var address = clean(selectors.address);
-
+  let result = yelp();
   // hooray! ES6 template literals!
-  console.log(`name: ${name}; address: ${address}`);
+  console.log(`name: ${result.name}; address: ${result.address}`);
+
+  let url = `${API_URL}?search=${result.name}`;
+  fetch(url)
+  .then(function(response) {
+    return response.blob();
+  })
+  .then(function(blob) {
+    console.log(blob);
+  });
+
 
 })();
